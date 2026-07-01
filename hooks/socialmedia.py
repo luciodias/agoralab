f4de7de413da0db4df2d08aeb6f8ac2e23809ffb
luciodias <email@email.com>
2026-07-01 10:13:13 -0300

    fix: add WhatsApp and Telegram sharing options to social media hooks

--- hooks/socialmedia.py
+++ hooks/socialmedia.py
@@ -4,7 +4,10 @@ import re
 
 x_intent = "https://twitter.com/intent/tweet"
 fb_sharer = "https://www.facebook.com/sharer/sharer.php"
+whatsapp = "https://wa.me"
+telegram = "https://t.me/share/url"
 include = re.compile(r"blog/[1-9].*")
+
 
 def on_page_markdown(markdown, **kwargs):
     page = kwargs["page"]
@@ -16,6 +19,9 @@ def on_page_markdown(markdown, **kwargs):
     page_title = urllib.parse.quote(page.title + "\n")
 
     return markdown + dedent(f"""
+    [Compartilhar no :simple-whatsapp:]({whatsapp}?text={page_title + urllib.parse.quote(page_url)}){{ .md-button }}
+    [Compartilhar no :simple-telegram:]({telegram}?url={page_url}&text={page_title}){{ .md-button }}
     [Compartilhar no :simple-x:]({x_intent}?text={page_title}&url={page_url}){{ .md-button }}
-    [Compartilhar no :simple-facebook:]({fb_sharer}?u={page_url}){{ .md-button }}
     """)
+
+    # [Compartilhar no :simple-facebook:]({fb_sharer}?u={page_url}){{ .md-button }}
